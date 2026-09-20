9f5f79cd35c01ad4d1e60152f8ad32a49d1a0608
LaurentiuStaicu <137863020+LaurentiuStaicu@users.noreply.github.com>
2026-09-20 11:12:22 +0300

    R3: expose bounded FTS5 retrieval scores

--- src/retrieval_query.h
+++ src/retrieval_query.h
@@ -17,25 +17,15 @@ typedef enum {
 #define ATM_RETRIEVAL_QUERY_ERROR \
     (atm_retrieval_query_error_quark ())
 
-typedef enum {
-    ATM_EVIDENCE_MATCH_EXACT,
-    ATM_EVIDENCE_MATCH_TABULAR,
-    ATM_EVIDENCE_MATCH_LEXICAL
-} AtmEvidenceMatch;
-
 typedef struct {
     char *evidence_kind;
     gint64 evidence_id;
-    char *repository_id;
-    char *repository_version;
-    char *snapshot_sha;
     char *logical_source_id;
     char *source_path;
     char *locator;
     char *title;
     char *body;
     guint source_roles;
-    AtmEvidenceMatch match_kind;
     gboolean has_lexical_score;
     double lexical_score;
 } AtmEvidenceRecord;
@@ -53,15 +43,6 @@ gboolean atm_retrieval_lookup_exact (
 gboolean atm_retrieval_search_fts (
     const char *index_path,
     const char *query,
-    guint max_results,
-    GPtrArray **out_results,
-    GError **error
-);
-
-gboolean atm_retrieval_lookup_dataset_rows (
-    const char *index_path,
-    const char *dataset_identifier,
-    const char *row_key,
     guint max_results,
     GPtrArray **out_results,
     GError **error
