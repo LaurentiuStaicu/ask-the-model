3deb18c24cc19071ae4d67f374189b4f2d1c2cd9
LaurentiuStaicu <137863020+LaurentiuStaicu@users.noreply.github.com>
2026-09-20 16:53:02 +0300

    R1: add narrow UI bridge for index lifecycle

---
+++ src/repository_ui_bridge.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+gboolean atm_repository_ui_ensure_index (
+    const char *cache_root,
+    const char *snapshot_root,
+    const char *repository_id,
+    const char *snapshot_sha,
+    char **out_index_path,
+    char **out_repository_version,
+    GError **error
+);
+
+G_END_DECLS
