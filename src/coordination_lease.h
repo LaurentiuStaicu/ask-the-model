6a731292a64af0235462a04bc9a3e8f03a0669a2
LaurentiuStaicu <137863020+LaurentiuStaicu@users.noreply.github.com>
2026-09-25 11:34:21 +0300

    OPT-B2: add shared/exclusive coordination lease mode

--- src/coordination_lease.h
+++ src/coordination_lease.h
@@ -9,10 +9,25 @@ typedef enum {
     ATM_COORDINATION_LEASE_ERROR_INVALID_OBJECT
 } AtmCoordinationLeaseError;
 
+typedef enum {
+    ATM_COORDINATION_LEASE_MODE_SHARED,
+    ATM_COORDINATION_LEASE_MODE_EXCLUSIVE
+} AtmCoordinationLeaseMode;
+
 #define ATM_COORDINATION_LEASE_ERROR \
     (atm_coordination_lease_error_quark ())
 
 GQuark atm_coordination_lease_error_quark (void);
+
+gboolean atm_coordination_lease_acquire_mode (
+    const char *path,
+    AtmCoordinationLeaseMode mode,
+    gboolean nonblocking,
+    gint *out_fd,
+    gboolean *out_contended,
+    gint64 *out_wait_us,
+    GError **error
+);
 
 gboolean atm_coordination_lease_acquire (
     const char *path,
