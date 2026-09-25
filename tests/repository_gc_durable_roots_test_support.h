3be456b62b374bc4b2d01d823eec2a9d9259d3a3
LaurentiuStaicu <137863020+LaurentiuStaicu@users.noreply.github.com>
2026-09-25 21:20:20 +0300

    Expose C1 symlink test helper

--- tests/repository_gc_durable_roots_test_support.h
+++ tests/repository_gc_durable_roots_test_support.h
@@ -9,4 +9,10 @@ gboolean atm_c1_test_publish_empty_complete_generation (
     GError **error
 );
 
+gboolean atm_c1_test_make_symlink (
+    const char *target,
+    const char *link_path,
+    GError **error
+);
+
 G_END_DECLS
